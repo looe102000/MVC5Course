9c10bbfd16ef0e93ce985c05af17c1eae95b9bdd
looe102000 <looe102000@gmail.com>
2017-05-14 09:38:23 +0800

    上送

---
+++ MVC5Course/Controllers/BaseController.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC5Course.Models;
+using MVC5Course.Models.ViewModels;
+
+namespace MVC5Course.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        protected FabricsEntities db = new FabricsEntities();
+
+        public ActionResult Debug()
+        {
+            return Content("Hello");
+        }
+
+        //protected override void HandleUnknownAction(string actionName)
+        //{
+        //    this.RedirectToAction("Index", "Home").ExecuteResult(this.ControllerContext);
+        //}
+    }
+}
---
+++ MVC5Course/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class HomeController : BaseController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Unknown()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult PartialAbout()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView("About");
+            }
+            else
+            {
+                return View("About");
+            }
+        }
+
+        public ActionResult SomeAction()
+        {
+            //Response.Write("<script>alert('建立成功!'); location.href='/';</script>");
+            //return "<script>alert('建立成功!'); location.href='/';</script>";
+            //return Content("<script>alert('建立成功!'); location.href='/';</script>");
+            return PartialView("SuccessRedirect", "/");
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Test()
+        {
+            return View();
+        }
+
+        public ActionResult GetFile()
+        {
+            return File(Server.MapPath("~/Content/WannaCry.png"), "image/png", "NewName.png");
+        }
+
+        public ActionResult GetJson()
+        {
+            db.Configuration.LazyLoadingEnabled = false;
+
+            return Json(db.Product.Take(5),
+                JsonRequestBehavior.AllowGet);
+        }
+    }
+}
